b1f8e514e5541dbfb0d212097d3c040027623906
Joshua Routledge <joshyrout@gmail.com>
2020-07-10 16:22:31 -0600

    Create PurchaseReport.java

---
+++ PurchaseReport.java
@@ -0,0 +1,19 @@
+//Joshua Routledge
+public class PurchaseReport {
+
+    public static void main(String[] args) {
+
+        System.out.print("=========================\n");
+        System.out.print("Purchase Report\n");
+        System.out.print("=========================\n");
+        System.out.printf("Shirt             $%,.2f\n",21.99);
+        System.out.printf("Pants             $%,.2f\n",49.97);
+        System.out.printf("Shoes             $%,.2f\n",89.50);
+        System.out.printf("Belt              $%,.2f\n",19.99);
+        System.out.printf("Coat             $%,.2f\n",129.95);
+        System.out.print("=========================\n");
+        System.out.printf("Total            $%,.2f%n",311.40);
+    }
+
+
+}
